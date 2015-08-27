4a4628032143ca978d3eb4e28322ebd1070b6845
micahk <legendofzeldafan3@yahoo.com>
2015-08-27 11:39:16 -0700

    Got menu mostly working, need to prevent cin from trying to use enter

--- HW01/HW01.cpp
+++ HW01/HW01.cpp
@@ -1,7 +1,7 @@
 /*
-	Name:
-	Date:
-	Assignment:
+	Name: Micah King
+	Date:8 /27/2015
+	Assignment: Create a menu
 	Description:	Create a menu to call the various functions.
 					Add comments for the functions...
 
@@ -26,8 +26,17 @@ void sizes()
 }
 
 void fourfours() {
-	cout << "44-44 = " << 44-44 << endl;
-	cout << "44/44 = " << 44/44 << endl; 
+	cout << "44-44 = " << 44-44 << endl;//0
+	cout << "44/44 = " << 44/44 << endl; //1
+	cout << "4/4 +4/4 = " << 4 / 4 + 4 / 4 << endl;//2
+	cout << "4 - 4/4 = " << 4 - 4 / 4 << endl;//3
+	cout << "4 = " << 4 << endl;//4
+	cout << "4 + 4/4 = " << 4 + 4 / 4 << endl;//5
+	cout << "4 + 4/4 + 4/4 = " << 4 + 4 / 4 + 4 / 4 << endl;//6
+	cout << "4+4 -4/4 = " << 4 + 4 - 4 / 4 << endl;//7
+	cout << "4+4 = " << 4 + 4 << endl;//8
+	cout << "4+4 +4/4 = " << 4 + 4 + 4 / 4 << endl;//9
+	cout << "4+4 +4/4 +4/4 = " << 4 + 4 + 4 / 4 + 4 / 4 << endl;//10
 }
 int multiplier(float x, float y) {
 	/*
@@ -48,16 +57,110 @@ float volCyl(float radius, float height) {
 	return PI * sqr(radius) * height;
 }
 
+int menu()
+{
+	char menucnt = 0;
+	int quit = 0;
+
+
+
+
+
+	while (quit == 0)
+	{
+		cout << "please select an option, or press q to quit" << endl;
+		cout << "\n 1) Sizes of types \n";
+		cout << "\n 2) Four Fours \n";
+		cout << "\n 3) Volume of a cylinder \n";
+		cout << "\n";
+		cin >> menucnt;
+		cout << "\n";
+
+
+		if (menucnt == 1 || menucnt == 2 || menucnt == 3)
+		{
+
+			if (menucnt == 1)
+			{
+				sizes();
+			}
+			else if (menucnt == 2)
+			{
+				fourfours();
+				menucnt = 0;
+			}
+			else if (menucnt == 3)
+			{
+				cout << "the volume of a cylinder is: " << volCyl(2, 5) << endl;
+				menucnt = 0;
+			}
+
+		}
+		else if (menucnt == 'q')
+		{
+			cout<< "quitting...";
+			quit = 1;
+		}
+
+		else if (menucnt == 0){}
+
+		else
+		{
+			cout << "You didn't enter a valid number";
+		}
+		
+	}
+
+
+
+
+
+
+
+	/*cout << "please select an option:" << endl;
+	cout << "\n 1) Sizes of types \n";
+	cout << "\n 2) Four Fours \n";
+	cout << "\n 3) Volume of a cylinder \n";
+	cout << "\n";
+	cin >> menucnt;
+	cout << "\n";
+	if (menucnt == 1 || menucnt == 2 || menucnt == 3)
+	{
+		if (menucnt ==1)
+		{ 
+			sizes();
+		}
+		else if (menucnt == 2)
+		{
+			fourfours();
+		}
+		else if (menucnt == 3)
+		{
+			cout << "the volume of a cylinder is: " << volCyl(2, 5) << endl;
+		}
+
+	}
+	else
+	{
+		cout << "You didn't enter a valid number";
+	}
+	*/
+	
+
+}
+
+
 int main() {
-	const bool T = 1;
+	/*const bool T = 1;
 	const bool F = 0;
 	sizes();
 	fourfours();
 	if(T) {
 		cout <<" it's true!" << endl;
 	}
-	cout << "the volume of a cylinder is: " << volCyl(2,5) << endl;
+	cout << "the volume of a cylinder is: " << volCyl(2,5) << endl;*/
 	
 	//Try implementing a menu here...
+	menu();
 	return 0;
 }
