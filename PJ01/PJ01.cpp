be459a7d1978f84fe8a51cd4f3607ece32db935d
micahk <legendofzeldafan3@yahoo.com>
2015-09-14 16:07:45 -0700

    Added in the computer choice code

--- PJ01/PJ01.cpp
+++ PJ01/PJ01.cpp
@@ -15,6 +15,8 @@ using namespace std;
 		
 	string arr[4];//0=playername,1=rounds played, 2= victories, 3=cheatactive
 	
+			
+	
 	
 	int randBetween() 
 	{		
@@ -48,8 +50,37 @@ using namespace std;
 	
 	void RPS()
 	{
-		int test = randmix();
-		cout << test<< endl;
+		int seedval = randmix();
+		int playerchoice;
+		srand(seedval);
+		int compchoice = rand()%13;
+		string compchoicestr="";
+		///begin computer choice code
+		if(compchoice ==1|| compchoice == -1|| compchoice==4|| compchoice==-4|| compchoice==7|| compchoice==-7||compchoice==0)
+		{
+			cout<<"1,4,7,0 choice:"<<compchoice<<endl;
+			compchoicestr= "rock";
+		}
+		else if(compchoice ==2|| compchoice == -2|| compchoice==5|| compchoice==-5|| compchoice==8|| compchoice==-8)
+		{
+			cout<<"2,5,8 choice:"<<compchoice<<endl;
+			compchoicestr = "scissors";
+		}
+		else if(compchoice ==3|| compchoice == -3|| compchoice==6|| compchoice==-6|| compchoice==9|| compchoice==-9)
+		{
+			cout<<"3,6,9 choice:"<<compchoice<<endl;
+			compchoicestr = "paper";
+		}
+		//end computer choice code
+		
+		
+		
+		
+		
+		
+		cout<<"Please enter rock, paper, or scissors: ";
+		cin>> playerchoice;
+		cout<< "\n"<<endl;
 	}
 	
 
@@ -61,11 +92,8 @@ using namespace std;
 void menu()
 	{
 		string menucnt ="";
-		cout << "You may press q to quit at anytime" << endl;
-		cout << "\n 1) Please enter your name: ";
-		cin>> arr[0];
-		cout<<arr[0]<<endl;
-		cout << "option selected: ";
+		
+		cout << "type start to begin, or quit to exit: ";
 		cin >> menucnt;//takes in input
 		cout << "\n";
 		
@@ -76,8 +104,9 @@ void menu()
 				RPS();
 				menu();//run menu method again
 			}
-			else if (menucnt == "other")
+			else if (menucnt == "quit")
 			{
+				break;
 			}
 			else
 			{
@@ -88,10 +117,18 @@ void menu()
 		
 		
 	}
+	void setup()
+	{
+		cout << "You may press q to quit at anytime" << endl;
+		cout << "\n 1) Please enter your name: ";
+		cin>> arr[0];
+		cout<<arr[0]<<endl;
+		menu();
+	}
 	int main()
 	{
 
-	   menu();//run menu code
+	   setup();//run menu code
 	   
         return 0;
 	}
